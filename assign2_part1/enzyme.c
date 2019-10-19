bfddef58e2d82981b72fa9b3e5fa99d9815a0156
tgrabham <tgrabham@sfu.ca>
2019-10-19 02:39:08 -0700

    fixed the memory leak in part1

--- assign2_part1/enzyme.c
+++ assign2_part1/enzyme.c
@@ -50,9 +50,10 @@ int make_enzyme_threads(pthread_t * enzymes, char *string, void *(*fp)(void *)) 
 	int i,rv,len;
 	thread_info_t *info;
 	len = strlen(string);
-	info = (thread_info_t *)malloc(sizeof(thread_info_t));
+
 
 	for(i=0;i<len-1;i++) {
+			info = (thread_info_t *)malloc(sizeof(thread_info_t));
 	    info->string = string+i;
 	    rv = pthread_create(enzymes+i,NULL,fp,info);
 	    if (rv) {
